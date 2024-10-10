cf09e8ab945d5bd7794882447713e92c07cc236d
Vrunda <vrundapatel692@gmail.com>
2024-10-10 19:06:29 -0400

    Add Python backend with Flask to handle data and connect to PostgreSQL

--- backend/app.py
+++ backend/app.py
@@ -1,22 +1,29 @@
 import os
 import psycopg2  # For PostgreSQL
 from flask import Flask, request, jsonify
+from dotenv import load_dotenv
+from flask_cors import CORS
+
+load_dotenv()  # Load environment variables from .env file
 
 app = Flask(__name__)
 
+CORS(app)
+
 # Database connection details
-DB_NAME = "mydb"
-DB_USER = "vrunda"
-DB_PASS = "Server@123"
-DB_HOST = "34.121.195.226"  # or "your_instance_connection_name" if using Cloud SQL
+DB_HOST = os.getenv('DB_HOST')
+DB_NAME = os.getenv('DB_NAME')
+DB_USER = os.getenv('DB_USER')
+DB_PASSWORD = os.getenv('DB_PASSWORD')
+
 
 # Establish the database connection
 conn = psycopg2.connect(
     dbname=DB_NAME,
     user=DB_USER,
-    password=DB_PASS,
-    host=DB_HOST
-)
+    password=DB_PASSWORD,
+    host=DB_HOST,
+    )
 
 @app.route('/submit', methods=['POST'])
 def submit_data():
@@ -27,7 +34,7 @@ def submit_data():
     if value1 and value2:
         # Insert data into the database
         with conn.cursor() as cursor:
-            cursor.execute("INSERT INTO your_table_name (column1, column2) VALUES (%s, %s)", (value1, value2))
+            cursor.execute("INSERT INTO data_table (value1, value2) VALUES (%s, %s)", (value1, value2))
             conn.commit()
         return jsonify({"message": "Data submitted successfully!"}), 200
     else:
@@ -35,3 +42,4 @@ def submit_data():
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
+
